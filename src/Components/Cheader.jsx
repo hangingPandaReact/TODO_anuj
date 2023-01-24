797f1b656abe23ec8e6650d945edf937497fa7f7
--global <--global>
2023-01-24 18:26:09 +0530

    Add todo task with validation and delete

---
+++ src/Components/Cheader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+class Cheader extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="heading"> My Todo-list</h1>
+        <p>Write your tasks here</p>
+      </div>
+    );
+  }
+}
+export default Cheader;
